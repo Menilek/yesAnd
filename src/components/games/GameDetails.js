365c2e4846173a5fafe9e440405c56fb0e9a0d0b
Menilek <menilek25@hotmail.co.uk>
2018-11-20 02:38:26 +0000

    Connected GameDetails with firestore to retrieve and display real data

--- src/components/games/GameDetails.js
+++ src/components/games/GameDetails.js
@@ -1,21 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 const GameDetails = (props) => {
-  const id = props.match.params.id;
-  return (
-    <div className="container section game-details">
-      <div className="card z-depth-0">
-        <div className="card-content">
-            <span className="card-title">Game Title - {id}</span>
-            <p>Lorem ipsum bla bla bla</p>
-        </div>
-        <div className="card-action grey lighten-4 grey-text">
-            <div>Posted by MT</div>
-            <div>19th Nov 16:30</div>
+  const { game } = props;
+  if(game) {
+    return (
+      <div className="container section game-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+              <span className="card-title">{ game.title }</span>
+              <p>{ game.content }</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+              <div>Posted by { game.authorFirstName } { game.authorLastName }</div>
+              <div>19th Nov 16:30</div>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }else{
+      return (
+        <p className="container centre">Loading game...</p>
+      )
+  }
 }
 
-export default GameDetails;
+const mapStateToProps = (state, localProps) => {
+  const id = localProps.match.params.id;
+  const games = state.firestore.data.games;
+  const game = games ? games[id] : null
+  return {
+    game: game
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'games' }
+  ])
+)
+(GameDetails);
--- src/components/games/GameList.js
+++ src/components/games/GameList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import GameSummary from './GameSummary';
+import { Link } from 'react-router-dom';
 
 const GameList = ({ games }) => {
     return(
         <div className="project-list section">
             {games && games.map(game => {
                 return(
-                    <GameSummary game={game} key={game.id} />
+                    <Link to={'/game/' + game.id}>
+                        <GameSummary game={game} key={game.id} />
+                    </Link>
                 )
             })}
         </div>
